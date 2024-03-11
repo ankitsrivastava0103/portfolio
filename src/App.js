9a3609988e35e914d8671c01d1b83ad804d36379
Ankit Srivastava <ankitsrivastava0103@gmail.com>
2024-03-11 18:31:02 +0530

    Setting Up TailWind CSS

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 function App() {
-  return <div>Test</div>;
+  return <div className="text-3xl font-bold underline">Test</div>;
 }
 
 export default App;
+
+// https://www.behance.net/gallery/113901239/Portfolio-Website-design
